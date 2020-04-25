b251cd9ad841028ade140e07ee84a4c43ca2cf9a
Captain-Ellie <54179498+Captain-Ellie@users.noreply.github.com>
2020-04-25 17:11:40 +0300

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,28 @@
 package com.company;
 import java.io.*;
 
-class Main {
-
+public class Main {
     public static void main(String[] args) {
-        try{
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String input = "";
             while (!input.equals("exit")) {
                 System.out.print("Type a sentence/word to check: ");
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 input = reader.readLine();
                 input = input.replaceAll("\\s+","")
                         .replaceAll("\\p{Punct}","")
                         .toLowerCase();
                 if (isPalindrome(input)) {
                     System.out.print("Palindrome\n");
-                }else {
-                    System.out.print("Not palindrome\n");
-                }
+                } else System.out.print("Not palindrome\n");
             }
-            
-        }catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-
     static boolean isPalindrome(String input) {
 
         int a = 0, z = input.length() - 1;
-        while(z > a) {
+        while (z > a) {
             if (input.charAt(a) != input.charAt(z)) {
                 return false;
             }
