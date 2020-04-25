fdd21f64c7c54a38b852b96a4cc1ab99156049ce
Captain-Ellie <54179498+Captain-Ellie@users.noreply.github.com>
2020-04-25 11:57:11 +0300

    edited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,31 @@
 package com.company;
 import java.io.*;
 
-class Main
-{
-    public static void main(String[] args) throws IOException
-    {
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
         String input = "";
         while (!input.equals("exit")) {
             System.out.print("Type a sentence/word to check: ");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            input = reader.readLine();
-            input = input.replaceAll("\\s+","");
-            input = input.replaceAll("\\p{Punct}","");
-            input = input.toLowerCase();
-            if (isPalindrome(input)) {
-                System.out.print("Palindrome\n");
-            }else {
-                System.out.print("Not palindrome\n");
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                input = reader.readLine();
+                input = input.replaceAll("\\s+","")
+                        .replaceAll("\\p{Punct}","")
+                        .toLowerCase();
+            }finally {
+                if (isPalindrome(input)) {
+                    System.out.print("Palindrome\n");
+                }else {
+                    System.out.print("Not palindrome\n");
+                }
             }
         }
     }
 
-    static boolean isPalindrome(String input)
-    {
+    static boolean isPalindrome(String input) {
+
         int a = 0, z = input.length() - 1;
         if (input.charAt(a) != input.charAt(z)) {
             return false;
